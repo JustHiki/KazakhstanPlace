bbf1f0254fb38137b23e27e7f57a38aa0fd50204
JustHiki <102988178+JustHiki@users.noreply.github.com>
2022-04-04 19:28:50 +0200

    Add files via upload

---
+++ hikiv2.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Kazakhstan flag - JustHiki
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  r/place flag
+// @author       JustHiki
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://styles.redditmedia.com/t5_4eiiz1/styles/communityIcon_ojy24r8j90o81.jpg
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://github.com/JustHiki/KazakhstanPlace/raw/main/overlay.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            console.log(i);
+            return i;
+        })())
+ 
+    }, false);
+ 
+}
